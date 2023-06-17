17b44d74a395696b6cd750b6ed71cf463db57e16
Loghin Vlad-Andrei <vlad.loghin@sonarsource.com>
2023-06-17 22:18:27 +0200

    Additions:
    main addition
    docs for clang-format
    tooling:
        clang format file
        clang format action
        sonarcloud action
    
    dependencies:
        cds
        gtest(cmake dep)

---
+++ src/target/visualizer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vlad-Andrei Loghin on 17.06.23.
+//
+
+int main(int argc, char const* const* argv) { return 0; }
---
+++ test/unittests/UnitTestsMain.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Vlad-Andrei Loghin on 17.06.23.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
