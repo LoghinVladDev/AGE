688a5c5308b5ec81689e50df2f45dc17a16623c0
Loghin Vlad-Andrei <vlad.loghin@sonarsource.com>
2023-06-18 04:53:49 +0200

    Additions:
    qt deps

--- src/target/visualizer.cpp
+++ src/target/visualizer.cpp
@@ -2,4 +2,13 @@
 // Created by Vlad-Andrei Loghin on 17.06.23.
 //
 
-int main(int argc, char const* const* argv) { return 0; }
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char** argv) {
+  QApplication app(argc, argv);
+  QWidget w;
+  w.resize(100, 100);
+  w.show();
+  return QApplication::exec();
+}
